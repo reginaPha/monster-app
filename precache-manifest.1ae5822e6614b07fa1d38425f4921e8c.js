65f57bfc18a40b9ac223de3925de6cbc245cbc57
YOUR NAME <mandana-s@hotmail.de>
2020-10-01 12:42:26 +0200

    Updates

---
+++ precache-manifest.1ae5822e6614b07fa1d38425f4921e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6978a42094a04ace1b31801f63dd2990",
+    "url": "/monster-app/index.html"
+  },
+  {
+    "revision": "7eaf956a50a625181e0d",
+    "url": "/monster-app/static/css/main.71d09d6f.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monster-app/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-app/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7eaf956a50a625181e0d",
+    "url": "/monster-app/static/js/main.dd4b9099.chunk.js"
+  },
+  {
+    "revision": "92a25797d6571872c4c3",
+    "url": "/monster-app/static/js/runtime-main.79c229cc.js"
+  }
+]);
